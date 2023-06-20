2419bb38e8f9631d27ebeb1f93bdc80c04cd27f3
Shuhrat Rahmonov <shuhrat@silkroadprofessionals.com>
2023-06-20 03:00:33 -0700

    User input

---
+++ 05.C# User Input/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What`s yout name?");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("What`s your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Your are  " + age + " Years old");
+        }
+    }
+}
