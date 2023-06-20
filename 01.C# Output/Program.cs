b6ff33d03d09e46a07a7b6bcdaae0a5ffbbc4854
Shuhrat Rahmonov <shuhrat@silkroadprofessionals.com>
2023-06-20 01:23:10 -0700

    Output

---
+++ 01.C# Output/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hey!");
+            Console.Write("Hello!");
+
+            //This is a comment
+            /*
+             * this
+             * is 
+             * a
+             * multiline
+             * comment
+             */
+
+            Console.WriteLine("Code");
+        }
+    }
+}
