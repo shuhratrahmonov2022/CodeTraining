4596fcb7f0705c7234ba4af94fff86ee94027735
Shuhratjon <shuhratrahmonov2022@gmail.com>
2023-06-22 22:12:25 +0500

    Arrays

---
+++ 20.C# Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Arrays
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            // array = a variable that can store multiple values, fixed size
+
+            //tring[] cars = { "BMW", "Mustang", "Corvette" };
+
+            String[] cars = new String[3];
+
+            cars[0] = "BMW";
+            cars[1] = "Mustang";
+            cars[2] = "Corvette";
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+        }
+    }
+}
