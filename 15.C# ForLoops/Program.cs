72b589db19308e540180444b922dd08689196849
Shuhratjon <shuhratrahmonov2022@gmail.com>
2023-06-22 09:46:20 +0500

    ForLoops

---
+++ 15.C# ForLoops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for loop = repeats some code a FINITE amount of times
+
+            //for (int index = 1; index <= 10; index+=2)
+            //{
+            //    Console.WriteLine(index);
+            //}
+
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("HAPpY NEW YEAR");
+        }
+    }
+}
