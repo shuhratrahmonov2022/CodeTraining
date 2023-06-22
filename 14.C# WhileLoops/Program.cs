eb385e8d54fba2d67af8e0a78f12781c2f36d88e
Shuhratjon <shuhratrahmonov2022@gmail.com>
2023-06-22 09:35:10 +0500

    WhileLoops

---
+++ 14.C# WhileLoops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "";
+
+            while (name == "")
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+            }
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
