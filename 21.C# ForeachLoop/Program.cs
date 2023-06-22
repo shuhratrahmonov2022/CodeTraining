4f6ae02b9847530e0931cc5381fc4e2b75ffb5ca
Shuhratjon <shuhratrahmonov2022@gmail.com>
2023-06-22 22:18:50 +0500

    ForeachLoop

---
+++ 21.C# ForeachLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ForeachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // foreach loop = a simpler way to iterate over an array, but it`s less flexible
+
+            String[] cars = { "BMW", "Mustang", "Corvette" };
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+            foreach (String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+
+}
