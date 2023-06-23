4a5358aef1e8d4ef35af85301b2c73787d6145d7
Shuhrat Rahmonov <shuhrat@silkroadprofessionals.com>
2023-06-22 23:58:52 -0700

    StringInterpolation

---
+++ 28.C# String Interpolation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            // string interpolation = allows us to insert variables into a string literal
+            //                        precede a string literal with $
+            //                        {} are placeholders
+
+            String firstName = "Code";
+            String LastName = "CodeC#";
+            int age = 21;
+
+            //Console.WriteLine("Hello " + firstName + " " + LastName + ".");
+            //Console.WriteLine("You are " + age + "years old.");
+
+            Console.WriteLine($"Hello {firstName} {LastName}.");
+            Console.WriteLine($"You are {age} old");
+        }
+    }
+}
