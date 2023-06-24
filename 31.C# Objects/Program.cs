cf4bff1a386e600a26720cacbdc51c3acf6a4fde
Shuhratjon <shuhratrahmonov2022@gmail.com>
2023-06-24 17:39:46 +0500

    Objects

---
+++ 31.C# Objects/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // object = An instance of a class
+            //          A class can be used as a blueprint to create objects (OOP)
+            //          objects can have fields & methods (characteristics & actions)
+
+            Human human1 = new Human();
+            Human human2 = new Human();
+            human1.name = "Shuhrat";
+            human1.age = 22;
+
+            human2.name = "Rahmonov";
+            human2.age = 22;
+
+            human1.Eat();
+            human1.Sleep();
+
+            human2.Eat();
+            human2.Sleep();
+        }
+    }
+
+    class Human
+    {
+        public string name;
+        public int age;
+
+        public void Eat()
+        {
+            Console.WriteLine(name + " is eating");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping");
+        }
+
+    }
+}
