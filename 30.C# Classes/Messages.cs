2572b77617a5d6f98355828f61931717e5cadc47
Shuhratjon <shuhratrahmonov2022@gmail.com>
2023-06-24 17:05:31 +0500

    Classes

---
+++ 30.C# Classes/Messages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Messages
+{
+    class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to the program");
+        }
+
+        public static void Waiting()
+        {
+            Console.WriteLine("I am waiting for something");
+        }
+
+        public static void bye()
+        {
+            Console.WriteLine("Bye! Thanks for visiting");
+        }
+    }
+}
