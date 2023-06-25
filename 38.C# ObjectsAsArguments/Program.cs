ca6f8630e4ffb8796795363c129bdbcfdd025b50
Shuhratjon <shuhratrahmonov2022@gmail.com>
2023-06-25 20:24:22 +0500

    ObjectsAsArguments

---
+++ 38.C# ObjectsAsArguments/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ObjectsAsArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Mustang","red");
+            Car car2 = Copy(car1);
+
+            Console.WriteLine(car2.color + " " + car2.model);
+
+        }
+        public static Car Copy(Car car)
+        {
+            return new Car(car.color, car.model);
+        }
+    }
+
+    class Car
+    {
+        public String model;
+        public String color;
+
+        public Car(string model, string color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+    }
+}
